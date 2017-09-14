db7ee4b40f44c3dafec13116343f71aa5d78cef4
Tito Yslas <roberto.yslas@gmail.com>
2017-09-14 16:30:36 -0600

    added Google Charts to create a graph instead of using D3

--- Public/gMaps.js
+++ Public/gMaps.js
@@ -1,3 +1,9 @@
+google.charts.load('current', {'packages':['bar']});
+google.charts.setOnLoadCallback(drawBasic);
+var magArr = [];
+var timeArr = [];
+var datesArr = [];
+
 var form = document.getElementById('srchParams');
 
 form.addEventListener('submit', function(e) {
@@ -46,9 +52,28 @@ form.addEventListener('submit', function(e) {
     .then(function(data) {
       var results = data;
 
+      var featArr = Array.from(data.features);
+      var featArrLen = featArr.length;
+
+      for (var i = 0; i < featArrLen; i++) {
+        datesArr.push(data.features[i].properties.time);
+      }
+      console.log('datesArr: ', datesArr);
+
+      for (var i = 0; i < featArrLen; i++) {
+        timeArr.push(new Date(data.features[i].properties.time));
+      }
+      console.log('timeArr: ', timeArr);
+
+
+      for (var j = 0; j < featArrLen; j++) {
+        magArr.push(data.features[j].properties.mag);
+      }
+      console.log('magArr: ', magArr);
+
   initMap(lat, lng, data);
 
-  initGraph(data);
+  drawBasic();
 
     });
 
@@ -119,26 +144,21 @@ function getCircle(magnitude) {
 function initGraph(data) {
   var w = 600;
   var h = 300;
-  var svg = d3.select('#timeline')
+  console.log(data);
+  d3.select('#timeline')
       .append('svg')
       .attr('width', w)
-      .attr('height', h);
+      .attr('height', h)
       // .attr('background-color', 'grey');
 
-  svg.selectAll('rect.colorBar')
+      .selectAll('rect.bar')
       .data(data)
       .enter()
       .append('rect')
       .attr('width', 20)
-      .attr('height', function(d, i) {
-        return d.features[i].properties.mag;
-      })
-      .attr('x', function() {
-        return i * 22;
-      })
-      .attr('y', function(d, i) {
-        return h - d.features[i].properties.mag;
-      })
+      .attr('height', 100)
+      .attr('x', 10)
+      .attr('y', 20)
       .attr('fill', 'black');
 
   var actTime = new Date(data.features[0].properties.time);
@@ -147,4 +167,48 @@ function initGraph(data) {
   // for (var i = 0; i < data.features.length; i++) {
   //   var seisTime = new Date(data.features[i].properties.time);
   // }
+}
+
+function drawBasic() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('date', 'Time of Event');
+    data.addColumn('number', 'Magnitude of Event');
+
+    console.log(timeArr[0]);
+
+
+    // write a for loop that loops through your array
+    // and data.addRows(for that data)
+    // then fix min/max range
+    for (var i = 0; i < magArr.length; i++) {
+      // console.log(magArr[i]);
+      // console.log(timeArr[i]);
+      var currDate = new Date(timeArr[i]);
+      data.addRows([
+              [new Date(currDate.getFullYear(), currDate.getMonth(), currDate.getDate()), magArr[i]],
+      ])
+    }
+    console.log(data.toJSON());
+
+    var options = {
+      chart: {
+        title: 'Seismic Events'
+      },
+      hAxis: {
+        title: 'Time of Event',
+        days: {format: ['MMM dd']},
+        viewWindow: {
+          min: new Date(2017, 08, 14, 0),
+          max: new Date(2017, 09, 16, 0)
+        }
+      },
+      vAxis: {
+        title: 'Magnitude'
+      }
+    };
+
+    var chart = new google.charts.Bar(
+      document.getElementById('chart_div'));
+
+    chart.draw(data, google.charts.Bar.convertOptions(options));
 }
